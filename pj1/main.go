ba40166df17aec772b658a93c1411f7b8e1539df
Liberbell <liberbell@gmail.com>
2021-10-04 08:43:12 +0900

    main1

--- pj1/main.go
+++ pj1/main.go
@@ -28,7 +28,7 @@ func main() {
 			fmt.Printf("File information: %s\n", r.MultipartForm)
 
 			file, _, _ := r.FormFile("file")
-			bytes, _, _ := ioutil.ReadAll(file)
+			bytes, _, := ioutil.ReadAll(file)
 		}
 	})
 }
