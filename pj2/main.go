68a284fdeacaa7fe70c7fe430764a86feb8aa7d9
Liberbell <liberbell@gmail.com>
2021-10-04 09:15:21 +0900

    main3

--- pj2/main.go
+++ pj2/main.go
@@ -8,6 +8,10 @@ import (
 func main() {
 	http.HandleFunc("/plaintext/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain")
-		io.WriteString()
+		io.WriteString(w, "<h1>This is a plain Text.</h1>")
+	})
+
+	http.HandleFunc("/html/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html")
 	})
 }
