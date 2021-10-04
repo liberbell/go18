75d0aebc9df08fa30f342153e08afb82f700b60e
Liberbell <liberbell@gmail.com>
2021-10-04 09:13:55 +0900

    main2

--- pj2/main.go
+++ pj2/main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"io"
+	"net/http"
+)
+
 func main() {
-	a
+	http.HandleFunc("/plaintext/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain")
+		io.WriteString()
+	})
 }
