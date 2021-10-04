96d4ec8a584ebc7bfcde45248cc4a4f32f1f7646
Liberbell <liberbell@gmail.com>
2021-10-04 09:30:11 +0900

    main3

--- pj3/main.go
+++ pj3/main.go
@@ -6,4 +6,8 @@ func main() {
 	http.HandleFunc("/notfound/", func(rw http.ResponseWriter, r *http.Request) {
 		rw.WriteHeader(404)
 	})
+
+	http.HandleFunc("/servererror/", func(rw http.ResponseWriter, r *http.Request) {
+		rw.WriteHeader(500)
+	})
 }
