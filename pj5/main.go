bfe73b476fc8d38f5eb9615eb74f4a0a0606f36a
Liberbell <liberbell@gmail.com>
2021-10-06 08:29:48 +0900

    main2

--- pj5/main.go
+++ pj5/main.go
@@ -16,11 +16,11 @@ func main() {
 	tmp1 := "<B><font color='green'>First Name: </font></B> {{.FirstName}} <BR> <B><font color='red'>Last Name: </font></B> {{.LastName}}"
 
 	router := httprouter.New()
-	router.GET("/name/:firstname/:lastname", func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	router.GET("/name/:firstName/:lastName", func(rw http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		t := template.New("NameTemplate")
 		tp, _ := t.Parse(tmp1)
 
-		tp.Execute(rw, &Person{ps.ByName("FirstName"), ps.ByName("Lastname")})
+		tp.Execute(rw, &Person{ps.ByName("firstName"), ps.ByName("lastname")})
 	})
 
 	http.ListenAndServe(":8001", nil)
