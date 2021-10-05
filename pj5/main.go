ef5734856259cda646d323cf102329c1450ef9ec
Liberbell <liberbell@gmail.com>
2021-10-05 09:30:09 +0900

    main5

--- pj5/main.go
+++ pj5/main.go
@@ -11,4 +11,7 @@ func main() {
 	tmp1 := "<B><font color='green'>First Name: </font></B> {{.FirstName}} <BR> <B><font color='red'>Last Name: </font></B>"
 
 	router := httprouter.New()
+	router.GET("/name/:firstname/:lastname", func(rw http.ResponseWriter, r *http.Request, p httprouter.Params ps httprouhttprouter.Param) {
+
+	})
 }
