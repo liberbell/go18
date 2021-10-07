e176752418260e6e21b86a2940ea785f777c42be
Liberbell <liberbell@gmail.com>
2021-10-07 09:26:23 +0900

    main6

--- pj9/main.go
+++ pj9/main.go
@@ -24,4 +24,11 @@ func main() {
 		people := []Person{Person{"Bob", "Smith"}, Person{"Larry", "Flint"}, Person{"Susan", "Sarandon"}, Person{"Brad", "Pitt"}, Person{"Betty", "White"}}
 		tp.Execute(rw, people)
 	})
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		tp, _ := template.ParseFiles(templateFile)
+		tp.Execute(rw, nil)
+	})
+
+	http.ListenAndServe(":8001", nil)
 }
