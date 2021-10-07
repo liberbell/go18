41d032a15bb697609bf9a7d683dc15d076cae75c
Liberbell <liberbell@gmail.com>
2021-10-07 09:24:00 +0900

    main4

--- pj9/main.go
+++ pj9/main.go
@@ -18,4 +18,9 @@ func main() {
 		people := []Person{Person{"Bob", "Smith"}, Person{"Larry", "Flint"}}
 		tp.Execute(rw, people)
 	})
+
+	http.HandleFunc("/longlist/", func(rw http.ResponseWriter, r *http.Request) {
+		tp, _ := template.ParseFiles(templateFile)
+		people := []Person{Person{"Bob", "Smith"}, Person{"Larry", "Flint"}, Person{"Susan", "Sarandon"}, Person{"Brad", "Pitt"}}
+	})
 }
