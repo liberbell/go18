b4fe923648fe85f08ea5a2d4ef9b13782e3870b1
Liberbell <liberbell@gmail.com>
2021-10-07 09:20:38 +0900

    main2

--- pj9/main.go
+++ pj9/main.go
@@ -1,10 +1,19 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
+)
+
 type Person struct {
 	FirstName string
 	LastName  string
 }
 
 func main() {
-	a
+	templateFile := "personlist.tmpl"
+
+	http.HandleFunc("/shortlist/", func(rw http.ResponseWriter, r *http.Request) {
+		tp, _ := template.ParseFiles(templateFile)
+	})
 }
