c39b6b013cb292bc42d65a695234d686fa85f3b0
Liberbell <liberbell@gmail.com>
2021-10-08 09:01:34 +0900

    main2

--- pj10/main.go
+++ pj10/main.go
@@ -1,8 +1,14 @@
 package main
 
+import "strings"
+
 type Person struct {
 	FirstName string
 	LastName  string
+}
+
+func removeSpace(s string) string {
+	return strings.ReplaceAll(s, " ", "")
 }
 
 func main() {
